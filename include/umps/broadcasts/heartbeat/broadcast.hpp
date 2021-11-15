e38fa02cb7acb00c67052f11958559bac185e01f
Ben Baker <bakerb845@gmail.com>
2021-11-15 12:52:55 -0700

    forgot some files

---
+++ include/umps/broadcasts/heartbeat/broadcast.hpp
@@ -0,0 +1,98 @@
+#ifndef UMPS_BROADCASTS_HEARTBEAT_BROADCAST_HPP
+#define UMPS_BROADCASTS_HEARTBEAT_BROADCAST_HPP
+#include <memory>
+#include "umps/broadcasts/broadcast.hpp"
+namespace UMPS
+{
+ namespace MessageFormats
+ {
+  class IMessage;
+ }
+ namespace Logging
+ {
+  class ILog;
+ }
+ namespace Services::ConnectionInformation
+ {
+  class Details;
+ }
+ namespace Broadcasts::Heartbeat
+ {
+  class Parameters;
+ }
+}
+namespace UMPS::Broadcasts::Heartbeat
+{
+/// @class Broadcast "broadcast.hpp" "umps/broadcasts/heartbeat/broadcast.hpp"
+/// @brief This defines the XPUB/XSUB proxy to broadcast heartbeats.
+///        This is an intermediate message layer to which producers
+///        can publish messages to this XSUB socket and from which
+///        subscribers can receive content from this XPUB socket.
+/// @copyright Ben Baker (University of Utah) distributed under the MIT license.
+class Broadcast : public IBroadcast
+{
+public:
+    /// @name Constructors
+    /// @{
+    /// @brief Constructs the XPUB/XSUB proxy.
+    Broadcast();
+    /// @brief Move constructor.
+    /// @param[in,out] broadcast  The broadcast from which to initialize 
+    ///                           this class.  On exit, broadcasts's behavior
+    ///                           is undefined.
+    Broadcast(Broadcast &&broadcast) noexcept;
+    /// @brief Constructs the XPUB/XSUB with a given logger.
+    explicit Broadcast(std::shared_ptr<UMPS::Logging::ILog> &logger);
+    /// @}
+
+    /// @name Operators
+    /// @{
+    /// @brief Move assignment operator.
+    /// @param[in,out] broadcast  The broadcast whose memory will be moved
+    ///                           to this.  On exit, broadcast's will be
+    ///                           undefined.
+    /// @result The memory from broadcast moved to this.
+    Broadcast& operator=(Broadcast &&broadcast) noexcept;
+    /// @}
+
+    /// @brief Initializes the proxy.
+    void initialize(const Parameters &parameters);
+
+    /// @name Implementations required for an IBroadcast
+    /// @{
+    /// @result True indicates that the broadcast is initialized.
+    [[nodiscard]] bool isInitialized() const noexcept override final;
+    /// @result The name of the broadcast.
+    [[nodiscard]] std::string getName() const override final;
+    /// @result The connection details.
+    [[nodiscard]] Services::ConnectionInformation::Details
+        getConnectionDetails() const override final;
+    /// @}
+
+    /// @name Starts the proxy.
+    /// @{
+    /// @brief Starts the proxy.
+    /// @throws std::runtime_error if \c isInitialized() is false.
+    void start() override final;
+    /// @result True indicates the proxy is running.
+    bool isRunning() const noexcept;
+    /// @brief Stops the proxy.
+    void stop() override final;
+    /// @}
+
+    /// @result An uninitialized instance of this class.
+    [[nodiscard]] std::unique_ptr<IBroadcast> createInstance() const noexcept override final;
+
+    /// @name Destructors
+    /// @{
+    virtual ~Broadcast();
+    /// @}
+
+    Broadcast(const Broadcast &broadcast);
+    Broadcast& operator=(const Broadcast &broadcast) = delete;
+private:
+    class BroadcastImpl;
+    std::unique_ptr<BroadcastImpl> pImpl;
+};
+};
+#endif
---
+++ include/umps/broadcasts/heartbeat/parameters.hpp
@@ -0,0 +1,109 @@
+#ifndef UMPS_BROADCASTS_HEARTBEAT_PARAMETERS_HPP
+#define UMPS_BROADCASTS_HEARTBEAT_PARAMETERS_HPP
+#include <memory>
+namespace UMPS::Broadcasts::Heartbeat
+{
+/// @class Parameters "parameters.hpp" "umps/broadcasts/heartbeat/parameters.hpp"
+/// @brief Defines the parameters for the underlying proxy socket that enables
+///        the packet broadcast.  Note the terminology - publishers connect
+///        to the frontend and subscribers connect to the backend so that
+///        data flows from front to back.
+/// @copyright Ben Baker (University of Utah) distributed under the MIT license.
+class Parameters
+{
+public:
+    /// @name Constructors
+    /// @{
+    /// @brief Constructor.
+    Parameters();
+    /// @brief Copy constructor.
+    /// @param[in] parameters  The parameters class from which to initialize 
+    ///                        this class.
+    Parameters(const Parameters &parameters);
+    /// @brief Move constructor.
+    /// @param[in,out] parameters  The parameters class from which to initialize
+    ///                            this class.  On exit, parameters's behavior
+    ///                            is undefined.
+    Parameters(Parameters &&parameters) noexcept;
+    /// @}
+
+    /// @name Operators
+    /// @{
+    /// @brief Copy assignment operator.
+    /// @param[in] parameters  The parameters class to copy to this.
+    /// @result A deep copy of the input parameters.
+    Parameters& operator=(const Parameters &parameters);
+    /// @brief Move assignment operator.
+    /// @param[in,out] parameters  The parameters class whose memory will
+    ///                            be moved to this.  On exit, parameters's
+    ///                            behavior is undefined.
+    /// @result The memory from parameters moved to this.
+    Parameters& operator=(Parameters &&parameters) noexcept;
+    /// @}
+
+    /// @name Connection Addresses
+    /// @{
+    /// @brief Sets the frontend's IP address.
+    /// @param[in] address  The address of the frontend.
+    /// @throws std::invalid_argument if this is empty.
+    void setFrontendAddress(const std::string &address);
+    /// @result The frontend's IP address.
+    /// @throws std::runtime_error if \c haveFrontendAddress() is false.
+    [[nodiscard]] std::string getFrontendAddress() const; 
+    /// @result True indicates the frontend address was set.
+    [[nodiscard]] bool haveFrontendAddress() const noexcept;
+
+    /// @brief Sets the backend's IP address.
+    /// @param[in] address  The address of the backend.
+    /// @throws std::invalid_argument if this is empty.
+    void setBackendAddress(const std::string &address);
+    /// @result The backend's IP address.
+    /// @throws std::runtime_error if \c haveBackendAddress() is false.
+    [[nodiscard]] std::string getBackendAddress() const;
+    /// @result True indicates the backend address was set.
+    [[nodiscard]] bool haveBackendAddress() const noexcept;
+    /// @}  
+
+    /// @name High-Water Mark
+    /// @{
+    /// @brief Sets the frontend's high water mark.
+    /// @param[in] highWaterMark  The approximate number of messages that can
+    ///                           be cached by the frontend.  Note, that setting
+    ///                           this to 0 effectively makes it infinite.
+    /// @throws std::invalid_argument if the high water mark is negative.
+    void setFrontendHighWaterMark(const int highWaterMark);
+    /// @result The high-water mark for the frontend.
+    [[nodiscard]] int getFrontendHighWaterMark() const noexcept;
+
+    /// @brief Sets the backend's high water mark.
+    /// @param[in] highWaterMark  The approximate number of messages that can
+    ///                           be cached by the backend.  Note, that setting
+    ///                           this to 0 effectively makes it infinite.
+    /// @throws std::invalid_argument if the high water mark is negative.
+    void setBackendHighWaterMark(const int highWaterMark);
+    /// @result The high-water mark for the backend.
+    [[nodiscard]] int getBackendHighWaterMark() const noexcept;
+    /// @}
+ 
+    /// @result The name of the broadcast.
+    [[nodiscard]] static std::string getName() noexcept;
+
+    /// @brief Loads parameters from an initialization file.
+    /// @param[in] iniFile  The name of the initialization file.
+    /// @param[in] section  The section of the ini file from which to
+    ///                     read variables.
+    void parseInitializationFile(const std::string &iniFile,
+                                 const std::string &section = "Heartbeat");
+    /// @name Destructors
+    /// @{
+    /// @brief Resets the class and releases memory.
+    void clear() noexcept;
+    /// @brief Destructor.
+    ~Parameters();
+    /// @}
+private:
+    class ParametersImpl;
+    std::unique_ptr<ParametersImpl> pImpl;
+};
+}
+#endif
