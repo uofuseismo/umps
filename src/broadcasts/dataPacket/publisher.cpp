f602381aa571a35eac6b432a223540caa5c28a8b
Ben Baker <bakerb845@gmail.com>
2022-01-04 11:31:01 -0700

    forgot a file

---
+++ src/broadcasts/dataPacket/publisher.cpp
@@ -0,0 +1,115 @@
+#include <zmq.hpp>
+#include "umps/broadcasts/dataPacket/publisher.hpp"
+#include "umps/broadcasts/dataPacket/publisherOptions.hpp"
+#include "umps/messageFormats/dataPacket.hpp"
+#include "umps/messaging/xPublisherXSubscriber/publisherOptions.hpp"
+#include "umps/messaging/xPublisherXSubscriber/publisher.hpp"
+#include "umps/messaging/authentication/service.hpp"
+#include "umps/messaging/authentication/authenticator.hpp"
+#include "umps/messaging/authentication/grasslands.hpp"
+#include "umps/logging/stdout.hpp"
+#include "private/staticUniquePointerCast.hpp"
+
+using namespace UMPS::Broadcasts::DataPacket;
+namespace UAuth = UMPS::Messaging::Authentication;
+namespace UXPubXSub = UMPS::Messaging::XPublisherXSubscriber;
+
+class Publisher::PublisherImpl
+{
+public:
+    PublisherImpl(std::shared_ptr<zmq::context_t> context,
+                  std::shared_ptr<UMPS::Logging::ILog> logger)
+    {
+        mPublisher = std::make_unique<UXPubXSub::Publisher> (context, logger);
+    }
+    std::unique_ptr<UXPubXSub::Publisher> mPublisher;
+    PublisherOptions mOptions;
+};
+
+/// C'tor
+Publisher::Publisher() :
+    pImpl(std::make_unique<PublisherImpl> (nullptr, nullptr))
+{
+}
+
+/// C'tor
+Publisher::Publisher(std::shared_ptr<zmq::context_t> &context) :
+    pImpl(std::make_unique<PublisherImpl> (context, nullptr))
+{
+}
+
+/// C'tor
+Publisher::Publisher(std::shared_ptr<UMPS::Logging::ILog> &logger) :
+    pImpl(std::make_unique<PublisherImpl> (nullptr, logger))
+{
+}
+
+/// C'tor
+Publisher::Publisher(std::shared_ptr<zmq::context_t> &context,
+                     std::shared_ptr<UMPS::Logging::ILog> &logger) :
+    pImpl(std::make_unique<PublisherImpl> (context, logger))
+{
+}
+
+/// Move c'tor
+Publisher::Publisher(Publisher &&publisher) noexcept
+{
+    *this = std::move(publisher);
+}
+
+/// Move assignment
+Publisher& Publisher::operator=(Publisher &&publisher) noexcept
+{
+    if (&publisher == this){return *this;}
+    pImpl = std::move(publisher.pImpl);
+    return *this;
+}
+
+/// Initialize
+void Publisher::initialize(const PublisherOptions &options)
+{
+    if (!options.haveAddress()){throw std::runtime_error("Address not set");}
+    auto publisherOptions = options.getPublisherOptions();
+    pImpl->mPublisher->initialize(publisherOptions);
+    pImpl->mOptions = options;
+}
+
+/// Initialized?
+bool Publisher::isInitialized() const noexcept
+{
+    return pImpl->mPublisher->isInitialized();
+}
+
+/// End point
+std::string Publisher::getEndPoint() const
+{
+    return pImpl->mPublisher->getEndPoint();
+}
+
+/// Security Level
+UAuth::SecurityLevel Publisher::getSecurityLevel() const noexcept
+{
+    return pImpl->mPublisher->getSecurityLevel();
+}
+
+/// Destructor
+Publisher::~Publisher() = default;
+
+/// Send
+template<typename U>
+void Publisher::send(const UMPS::MessageFormats::DataPacket<U> &message)
+{
+    pImpl->mPublisher->send(message); 
+}
+
+///--------------------------------------------------------------------------///
+///                             Template Instantiation                       ///
+///--------------------------------------------------------------------------///
+template void UMPS::Broadcasts::DataPacket::Publisher::send(
+    const UMPS::MessageFormats::DataPacket<double> &message);
+template void UMPS::Broadcasts::DataPacket::Publisher::send(
+    const UMPS::MessageFormats::DataPacket<float> &message);
+template void UMPS::Broadcasts::DataPacket::Publisher::send(
+    const UMPS::MessageFormats::DataPacket<int> &message);
+template void UMPS::Broadcasts::DataPacket::Publisher::send(
+    const UMPS::MessageFormats::DataPacket<int16_t> &message);
---
+++ src/broadcasts/dataPacket/publisherOptions.cpp
@@ -0,0 +1,122 @@
+#include <string>
+#include <chrono>
+#include "umps/broadcasts/dataPacket/publisherOptions.hpp"
+#include "umps/messaging/xPublisherXSubscriber/publisherOptions.hpp"
+#include "umps/messaging/authentication/zapOptions.hpp"
+
+using namespace UMPS::Broadcasts::DataPacket;
+
+class PublisherOptions::PublisherOptionsImpl
+{
+public:
+    PublisherOptionsImpl()
+    {
+        mOptions.setHighWaterMark(512); // No point making this infinite
+        mOptions.setTimeOut(std::chrono::milliseconds{1000}); 
+    }
+    UMPS::Messaging::XPublisherXSubscriber::PublisherOptions mOptions;
+};
+
+/// C'tor
+PublisherOptions::PublisherOptions() :
+    pImpl(std::make_unique<PublisherOptionsImpl> ())
+{
+}
+
+/// Copy c'tor
+PublisherOptions::PublisherOptions(const PublisherOptions &options)
+{
+    *this = options;
+}
+
+/// Move c'tor
+PublisherOptions::PublisherOptions(PublisherOptions &&options) noexcept
+{
+    *this = std::move(options);
+}
+
+/// Destructor
+PublisherOptions::~PublisherOptions() = default;
+
+/// Reset class
+void PublisherOptions::clear() noexcept
+{
+    pImpl = std::make_unique<PublisherOptionsImpl> ();
+}
+
+/// Copy assignment
+PublisherOptions&
+    PublisherOptions::operator=(const PublisherOptions &options)
+{
+    if (&options == this){return *this;}
+    pImpl = std::make_unique<PublisherOptionsImpl> (*options.pImpl);
+    return *this;
+}
+
+/// Move assignment
+PublisherOptions&
+    PublisherOptions::operator=(PublisherOptions &&options) noexcept
+{
+    if (&options == this){return *this;}
+    pImpl = std::move(options.pImpl);
+    return *this;
+}
+
+/// High water mark
+void PublisherOptions::setHighWaterMark(const int hwm)
+{
+    pImpl->mOptions.setHighWaterMark(hwm);
+}
+
+int PublisherOptions::getHighWaterMark() const noexcept
+{
+    return pImpl->mOptions.getHighWaterMark();
+}
+
+/// Address
+void PublisherOptions::setAddress(const std::string &address)
+{
+    pImpl->mOptions.setAddress(address);
+}
+
+std::string PublisherOptions::getAddress() const
+{
+    return pImpl->mOptions.getAddress();
+}
+
+bool PublisherOptions::haveAddress() const noexcept
+{
+    return pImpl->mOptions.haveAddress();
+}
+
+/// ZAP options
+void PublisherOptions::setZAPOptions(
+    const UMPS::Messaging::Authentication::ZAPOptions &options)
+{
+    pImpl->mOptions.setZAPOptions(options);
+} 
+
+UMPS::Messaging::Authentication::ZAPOptions 
+    PublisherOptions::getZAPOptions() const noexcept
+{
+    return pImpl->mOptions.getZAPOptions();
+}
+
+/// Get the options
+UMPS::Messaging::XPublisherXSubscriber::PublisherOptions
+    PublisherOptions::getPublisherOptions() const noexcept
+{
+    return pImpl->mOptions;
+}
+
+/// Timeout
+void PublisherOptions::setTimeOut(
+    const std::chrono::milliseconds timeOut) noexcept
+{
+    pImpl->mOptions.setTimeOut(timeOut);
+}
+
+std::chrono::milliseconds PublisherOptions::getTimeOut() const noexcept
+{
+    return pImpl->mOptions.getTimeOut();
+}
