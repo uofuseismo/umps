f1c312b4e59c6a8b48f1552d937ae42445852bf7
Ben Baker <bakerb845@gmail.com>
2022-08-03 15:32:33 -0600

    removing zmq context

---
+++ include/umps/messaging/context.hpp
@@ -0,0 +1,31 @@
+#ifndef UMPS_MESSAGING_CONTEXT_HPP
+#define UMPS_MESSAGING_CONTEXT_HPP
+#include <memory>
+#include <cstdint>
+namespace UMPS::Messaging
+{
+/// @class Context context.hpp "umps/messaging/context.hpp"
+/// @brief This is a wrapper around the ZeroMQ context.
+/// @copyright Ben Baker (University of Utah) distributed under the MIT license.
+class Context
+{
+public:
+    /// @brief Constructor. 
+    Context();
+    /// @brief Constructor.
+    /// @param[in] nInputOutputThreads  The number of input/output threads.
+    ///                                 This is typically 1 for each Gb/s
+    ///                                 of communication.  However, if this is
+    ///                                 for IPC communication this can be 0.
+    explicit Context(int nInputOutputThreads);
+    /// @brief Destructor.
+    ~Context();
+
+    /// @result A pointer to the ZeroMQ context.
+    std::uintptr_t getContext() const;
+private:
+    class ContextImpl;
+    std::unique_ptr<ContextImpl> pImpl; 
+};
+}
+#endif
