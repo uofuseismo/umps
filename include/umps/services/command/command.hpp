27e969ef83c516310f8b5a44e6ad5ecad100d330
Ben Baker <bakerb845@gmail.com>
2024-10-15 10:49:24 -0600

    adding a convenience include

---
+++ include/umps/services/command/command.hpp
@@ -0,0 +1,15 @@
+#ifndef UMPS_SERVICES_COMMAND_HPP
+#define UMPS_SERVICES_COMMAND_HPP
+#include <command/availableCommandsRequest.hpp>
+#include <command/availableCommandsResponse.hpp>
+#include <command/commandRequest.hpp>
+#include <command/commandResponse.hpp>
+#include <command/moduleDetails.hpp>
+#include <command/moduleTable.hpp>
+#include <command/requestor.hpp>
+#include <command/requestorOptions.hpp>
+#include <command/service.hpp>
+#include <command/serviceOptions.hpp>
+#include <command/terminateRequest.hpp>
+#include <command/terminateResponse.hpp>
+#endif
