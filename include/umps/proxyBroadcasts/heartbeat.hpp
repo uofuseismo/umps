62e4870ff0cb3927dfd4c306717fbe8284897f16
Ben Baker <bakerb845@gmail.com>
2024-10-29 13:10:51 -0600

    adding a conveneince include for heartbeat

---
+++ include/umps/proxyBroadcasts/heartbeat.hpp
@@ -0,0 +1,10 @@
+#ifndef UMPS_PROXY_BROADCASTS_HEARTBEAT_HPP
+#define UMPS_PROXY_BROADCASTS_HEARTBEAT_HPP
+#include <umps/proxyBroadcasts/heartbeat/publisher.hpp>
+#include <umps/proxyBroadcasts/heartbeat/publisherOptions.hpp>
+#include <umps/proxyBroadcasts/heartbeat/publisherProcess.hpp>
+#include <umps/proxyBroadcasts/heartbeat/publisherProcessOptions.hpp>
+#include <umps/proxyBroadcasts/heartbeat/status.hpp>
+#include <umps/proxyBroadcasts/heartbeat/subscriber.hpp>
+#include <umps/proxyBroadcasts/heartbeat/subscriberOptions.hpp>
+#endif
